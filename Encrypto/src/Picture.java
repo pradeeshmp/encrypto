540c2d52e58adc15a8a85a6efd498477e0cb7052
dinesh.cape@gmail.com <dinesh.cape@gmail.com@5d7cf094-cb0c-5ec9-2541-145cabed92cd>
2011-09-01 09:05:22 +0000

    Initial commit

---
+++ Encrypto/src/Picture.java
@@ -0,0 +1,178 @@
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.ImageIcon;
+import javax.swing.KeyStroke;
+import java.awt.FileDialog;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+
+public final class Picture implements ActionListener 
+{
+    private BufferedImage image;    // the rasterized image
+    private JFrame frame;           // on-screen view
+    private String filename;        // name of file
+
+    public Picture(int w, int h) {
+        image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        // set to TYPE_INT_ARGB to support transparency
+        filename = w + "-by-" + h;
+    }
+
+    public String toString(){
+    	return filename;
+    }
+    
+    public Picture(String filename) 
+    {
+        this.filename = filename;
+        try {
+            // try to read from file in working directory
+            File file = new File(filename);
+            if (file.isFile()) {
+                image = ImageIO.read(file);
+            }
+
+            // now try to read from file in same directory as this .class file
+            else {
+                URL url = getClass().getResource(filename);
+                if (url == null) { url = new URL(filename); }
+                image = ImageIO.read(url);
+            }
+        }
+        catch (IOException e) {
+            // e.printStackTrace();
+            throw new RuntimeException("Could not open file: " + filename);
+        }
+
+        // check that image was read in
+        if (image == null) {
+            throw new RuntimeException("Invalid image file: " + filename);
+        }
+    }
+
+   
+    public Picture(File file) 
+    {
+        try { image = ImageIO.read(file); }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not open file: " + file);
+        }
+        if (image == null) {
+            throw new RuntimeException("Invalid image file: " + file);
+        }
+    }
+
+   
+    public JLabel getJLabel() {
+        if (image == null) { return null; }         // no image available
+        ImageIcon icon = new ImageIcon(image);
+        return new JLabel(icon);
+    }
+
+   
+    public void show() 	
+    {
+
+        // create the GUI for viewing the image if needed
+        if (frame == null) {
+            frame = new JFrame();
+
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+            menuBar.add(menu);
+            JMenuItem menuItem1 = new JMenuItem(" Save...   ");
+            menuItem1.addActionListener(this);
+            menuItem1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+                                     Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+            menu.add(menuItem1);
+            frame.setJMenuBar(menuBar);
+
+
+
+            frame.setContentPane(getJLabel());
+            // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setTitle(filename);
+            frame.setResizable(false);
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+        // draw
+        frame.repaint();
+    }
+
+   
+    public int height() 
+    {
+        return image.getHeight(null);
+    }
+
+   
+    public int width() 
+    {
+        return image.getWidth(null);
+    }
+
+   
+    public Color get(int i, int j) 
+    {
+        return new Color(image.getRGB(i, j));
+    }
+
+   
+    public void set(int i, int j, Color c) {
+        if (c == null) { throw new RuntimeException("can't set Color to null"); }
+        image.setRGB(i, j, c.getRGB());
+    }
+
+   
+    public void save(String name) 
+    {
+        save(new File(name));
+    }
+
+   
+    public void save(File file) 
+    {
+        this.filename = file.getName();
+        if (frame != null) { frame.setTitle(filename); }
+        String suffix = filename.substring(filename.lastIndexOf('.') + 1);
+        suffix = suffix.toLowerCase();
+        if (suffix.equals("jpg") || suffix.equals("png")) 
+        {
+            try { ImageIO.write(image, suffix, file); }
+            catch (IOException e) { e.printStackTrace(); }
+        }
+        else 
+        {
+            System.out.println("Error: filename must end in .jpg or .png");
+        }
+    }
+
+   
+    public void actionPerformed(ActionEvent e) 
+    {
+        FileDialog chooser = new FileDialog(frame,
+                             "Use a .png or .jpg extension", FileDialog.SAVE);
+        chooser.setVisible(true);
+        if (chooser.getFile() != null) 
+        {
+            save(chooser.getDirectory() + File.separator + chooser.getFile());
+        }
+    }
+}
